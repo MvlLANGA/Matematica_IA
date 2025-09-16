c58c6de05e35bcaabfda9eec9861effc23af149b
Marcos Vinicius <langa.langa1984@gmail.com>
2025-09-16 16:46:43 -0300

    Matematica para IA

---
+++ exercicio8.py
@@ -0,0 +1,23 @@
+import numpy as np 
+import statistics
+import matplotlib.pyplot as plt
+import random
+from collections import Counter
+
+numeros = [12,15,12,18,20,15,22,19,15,10]
+print("O desvio padrão é: ", np.std(numeros))
+print("A variancia é: ", np.var(numeros))
+
+print("A media é: ", statistics.mean(numeros))
+print("A mediana é: ", statistics.median(numeros))
+print("A moda é: ", statistics.multimode(numeros))
+
+frequencia =Counter(numeros)
+print("Frequencia: ",frequencia)
+
+#plt.hist(numeros, bins=range(min(numeros), max(numeros)+2), color="green", edgecolor="black")
+plt.hist(numeros,color='green', edgecolor='black')
+plt.title("Histograma Numeros")
+plt.xlabel("numeros")
+plt.ylabel("frequencia")
+plt.show()
